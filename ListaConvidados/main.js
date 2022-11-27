bef2ee58fcee49e90ef3ea49dd42dba0f3571e11
Yuri Alexandre <116982947+YuriAPCarvalho@users.noreply.github.com>
2022-11-27 01:35:17 -0400

    listaConvidados

---
+++ ListaConvidados/main.js
@@ -0,0 +1,39 @@
+let convidados = []
+
+function inserir() {
+    let nomeConvidado = document.getElementById("nomeConvidado").value
+
+    if (nomeConvidado == "") {
+        alert("Preencha o nome do convidado!")
+    } else {
+        convidados.push(nomeConvidado)
+        limpar()
+        imprimir()
+
+    }
+}
+
+function imprimir() {
+    let tabela = document.getElementById("tabela")
+    let cacheTabela = ""
+    tabela.innerHTML = ""
+
+    for (let i = 0; i < convidados.length; i++) {
+
+        cacheTabela += `<tr><td>${convidados[i]}</td><td><img src="apagar.png" onclick="remover(${i})"/></td></tr>`
+    }
+    tabela.innerHTML = cacheTabela
+}
+
+function limpar() {
+    document.getElementById("nomeConvidado").value = ""
+}
+
+function remover(posicao) {
+    if (confirm("Tem certeza que deseja remover este convidado?") == true )  {
+        convidados.splice(posicao, 1)
+        imprimir()
+    }
+}
+
+
